47d9b9873d72095261c12a7c3b408551b64748a1
navdeep <navis12.ns@gmail.com>
2024-07-13 23:06:43 +0530

    Update page.tsx

--- dsa_project/app/dashboard/platformRatings/page.tsx
+++ dsa_project/app/dashboard/platformRatings/page.tsx
@@ -54,11 +54,11 @@ export default function PlatformRatings(){
     
     
     return(
-        <div className="overflow-y-auto h-screen grid grid-cols-1">
+        <div className=" h-screen mt-1">
              <div className="text-2xl text-white font-semibold mb-2 ">
                 Platform Ratings
              </div>
-             <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 lg:grid-cols-4 sm:mb-0 mb-20">
+             <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 lg:grid-cols-4 sm:mb-0 mb-20 overflow-y-auto">
               <CodechefCard />
              <CodeforcesCard />
               {leetcode_username? <LeetcodeCard userName={leetcode_username} />:<div className="skeleton-loader  h-80 bg-cardBlue-custom rounded-lg shadow-md p-6 shadow-md p-6 animate-pulse">
