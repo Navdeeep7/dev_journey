e669e53d3f0d2f92f9d9fe1b0798c1a784c70bb1
Navdeep <navis12.ns@gmail.com>
2024-08-31 16:29:31 +0530

    Update route.ts

--- dsa_project/app/api/allProjects/route.ts
+++ dsa_project/app/api/allProjects/route.ts
@@ -5,6 +5,10 @@ import { headers } from 'next/headers';
 
 export async function GET(req:NextRequest){
     await connectToDb();
+    const headers = new Headers();
+    headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    headers.set('Pragma', 'no-cache');
+    headers.set('Expires', '0');
     const projects=await Project.find();
     return NextResponse.json(projects);
 }
