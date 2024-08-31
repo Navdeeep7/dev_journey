2941ef70d283f6eaeb2d0a39f21b311b23dfafed
Navdeep <navis12.ns@gmail.com>
2024-08-31 19:45:46 +0530

    Update page.tsx

--- dsa_project/app/dashboard/exploreProjects/page.tsx
+++ dsa_project/app/dashboard/exploreProjects/page.tsx
@@ -19,7 +19,7 @@ export default function ExploreProjects(){
             <div className="text-2xl text-white font-semibold mb-2 ml-2 ">
                 Explore Projects 
             </div>
-           {loading ? <div> </div> : <div> {projects.map((p:any,key:any)=><AllProjectCard key={key} project={p}/>)}</div>}
+           {loading ? <div> </div> : <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6"> {projects.map((p:any,key:any)=><AllProjectCard key={key} project={p}/>)}</div>}
 
             </div>
         </div>
