705a7aa5318153e4fab853d56644709c9bae7d5d
Navdeep <navis12.ns@gmail.com>
2024-08-31 20:39:53 +0530

    Update route.ts

--- dsa_project/app/api/allProjects/route.ts
+++ dsa_project/app/api/allProjects/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
+
 import { Project, User, connectToDb } from "@/db";
+
 import crypto from 'crypto';
 
 export async function GET(req: NextRequest) {
@@ -8,9 +10,7 @@ export async function GET(req: NextRequest) {
   headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
   headers.set('Pragma', 'no-cache');
   headers.set('Expires', '0');
-  const project = await Project.find();
-  const timestamp = new Date().getTime(); // include a timestamp in the data
-  const projects = { project, timestamp };
+  const projects = await Project.find();
   const dataHash = crypto.createHash('sha256').update(JSON.stringify(projects)).digest('hex');
   headers.set('ETag', `"${dataHash}"`);
   headers.set('Last-Modified', new Date().toUTCString());
