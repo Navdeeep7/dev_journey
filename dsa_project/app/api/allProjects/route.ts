2b2c96d46c5e2c9a0bf6a8509d72d3fb119c3d6e
Navdeep <navis12.ns@gmail.com>
2024-08-31 20:12:50 +0530

    Update route.ts

--- dsa_project/app/api/allProjects/route.ts
+++ dsa_project/app/api/allProjects/route.ts
@@ -1,17 +1,18 @@
-import { NextRequest,NextResponse } from "next/server";
-import jwt from "jsonwebtoken";
-import {Project, User,connectToDb} from "@/db"
-import { headers } from 'next/headers';
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req:NextRequest){
-    await connectToDb();
-    const headers = new Headers();
-    headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
-    headers.set('Pragma', 'no-cache');
-    headers.set('Expires', '0');
-    const projects=await Project.find();
-    const etag = Math.random().toString(36).substring(2, 15);
-    headers.set('ETag', `"${etag}"`);
-    headers.set('Last-Modified', new Date().toUTCString());
-    return NextResponse.json(projects);
+import { Project, User, connectToDb } from "@/db";
+
+import crypto from 'crypto';
+
+export async function GET(req: NextRequest) {
+  await connectToDb();
+  const headers = new Headers();
+  headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  headers.set('Pragma', 'no-cache');
+  headers.set('Expires', '0');
+  const projects = await Project.find();
+  const dataHash = crypto.createHash('sha256').update(JSON.stringify(projects)).digest('hex');
+  headers.set('ETag', `"${dataHash}"`);
+  headers.set('Last-Modified', new Date().toUTCString());
+  return NextResponse.json(projects);
 }
